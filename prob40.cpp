a9880df2dfd639ea79ef897a9ed0d1753d4e262c
manay-ysabella <yemanay@mymail.mapua.edu.ph>
2024-09-09 23:25:23 +0800

    Update prob40.cpp

--- prob40.cpp
+++ prob40.cpp
@@ -4,7 +4,6 @@ using namespace std;
 int main() {
     int a, b, c;
 
-    // Input three numbers
     cout << "Enter first number: ";
     cin >> a;
     cout << "Enter second number: ";
@@ -12,15 +11,13 @@ int main() {
     cout << "Enter third number: ";
     cin >> c;
 
-    // Check if inputs are within the valid range (0-9999)
     if ((a < 0 || a > 9999) || (b < 0 || b > 9999) || (c < 0 || c > 9999)) {
         cout << "Invalid input! Numbers should be between 0 and 9999." << endl;
     } else {
-        // Calculate the last digits of (a * b) and c
+
         int lastDigitProduct = (a * b) % 10;
         int lastDigitC = c % 10;
 
-        // Check if the last digits are equal
         if (lastDigitProduct == lastDigitC) {
             cout << "TRUE" << endl;
         } else {
